aa3ce5e7766c84904896d1d0ad831d7e6274d231
sadness_ojisan <sadness.ojisan@gmail.com>
2020-10-07 03:05:40 +0900

    不要なexportをindexから削除

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
-export { render, hydrate } from './render';
-export {
-	createElement,
-	createElement as h,
-	Fragment,
-	createRef,
-	isValidElement
-} from './create-element';
-export { Component } from './component';
-export { cloneElement } from './clone-element';
-export { createContext } from './create-context';
-export { toChildArray } from './diff/children';
-export { unmount as __u } from './diff';
-export { default as options } from './options';
+export { render } from './render';
+export { createElement, createElement as h } from './create-element';
