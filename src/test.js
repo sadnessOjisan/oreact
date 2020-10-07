a47f5d0c77de4afe4c0b7c95ab2115cc66815ce3
sadness_ojisan <sadness.ojisan@gmail.com>
2020-10-07 14:58:30 +0900

    rm tet.js

--- src/test.js
+++
@@ -1,9 +0,0 @@
-const { createElement, render } = require('../dist/preact');
-
-console.log(createElement('div', null, null));
-document = {
-	createElement: () => {}
-};
-
-const vdom = createElement('div', {}, {});
-console.log(render(vdom, {}, {}));
