ad0fbf5eb9e94d79dc0b362202f70e092a17c6ab
sadness_ojisan <sadness.ojisan@gmail.com>
2020-10-15 03:07:45 +0900

    rm

--- js/constants.js
+++
@@ -1,3 +0,0 @@
-export var EMPTY_OBJ = {};
-export var EMPTY_ARR = [];
-export var IS_NON_DIMENSIONAL = /acit|ex(?:s|g|n|p|$)|rph|grid|ows|mnc|ntw|ine[ch]|zoo|^ord|itera/i;
--- packages/oreact/lib/oreact.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = oreact;
-
-function oreact() {
-    // TODO
-}
