c143d967c7fb35dc03f214a0d4b8e3232c1268a7
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-01 06:11:20 +0900

    delete replaceNode from render

--- packages/oreact/js/render.js
+++ packages/oreact/js/render.js
@@ -10,25 +10,18 @@ const IS_HYDRATE = EMPTY_OBJ;
  * @param {import('./index').ComponentChild} vnode The virtual node to render
  * @param {import('./internal').PreactElement} parentDom The DOM element to
  * render into
- * @param {Element | Text} [replaceNode] Optional: Attempt to re-use an
- * existing DOM tree rooted at `replaceNode`
  */
-export function render(vnode, parentDom, replaceNode) {
+export function render(vnode, parentDom) {
 	if (options._root) options._root(vnode, parentDom);
 
-	// We abuse the `replaceNode` parameter in `hydrate()` to signal if we
-	// are in hydration mode or not by passing `IS_HYDRATE` instead of a
-	// DOM element.
-	let isHydrating = replaceNode === IS_HYDRATE;
+	let isHydrating = false;
 
 	// To be able to support calling `render()` multiple times on the same
 	// DOM node, we need to obtain a reference to the previous tree. We do
 	// this by assigning a new `_children` property to DOM nodes which points
 	// to the last rendered tree. By default this property is not present, which
 	// means that we are mounting a new tree for the first time.
-	let oldVNode = isHydrating
-		? null
-		: (replaceNode && replaceNode._children) || parentDom._children;
+	let oldVNode = isHydrating ? null : parentDom._children;
 	vnode = createElement(Fragment, null, [vnode]);
 
 	// List of effects that need to be called after diffing.
@@ -37,19 +30,17 @@ export function render(vnode, parentDom, replaceNode) {
 		parentDom,
 		// Determine the new vnode tree and store it on the DOM element on
 		// our custom `_children` property.
-		((isHydrating ? parentDom : replaceNode || parentDom)._children = vnode),
+		((isHydrating ? parentDom : parentDom)._children = vnode),
 		oldVNode || EMPTY_OBJ,
 		EMPTY_OBJ,
 		parentDom.ownerSVGElement !== undefined,
-		replaceNode && !isHydrating
-			? [replaceNode]
-			: oldVNode
+		oldVNode
 			? null
 			: parentDom.childNodes.length
 			? EMPTY_ARR.slice.call(parentDom.childNodes)
 			: null,
 		commitQueue,
-		replaceNode || EMPTY_OBJ,
+		EMPTY_OBJ,
 		isHydrating
 	);
 
