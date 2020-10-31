6c1754b7ff3432faa8c3aaf23f8cc77d0163dcca
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-01 06:03:03 +0900

    rm toChildArray

--- packages/oreact/js/diff/children.js
+++ packages/oreact/js/diff/children.js
@@ -228,25 +228,6 @@ export function diffChildren(
 	}
 }
 
-/**
- * Flatten and loop through the children of a virtual node
- * @param {import('../index').ComponentChildren} children The unflattened
- * children of a virtual node
- * @returns {import('../internal').VNode[]}
- */
-export function toChildArray(children, out) {
-	out = out || [];
-	if (children == null || typeof children == 'boolean') {
-	} else if (Array.isArray(children)) {
-		children.some(child => {
-			toChildArray(child, out);
-		});
-	} else {
-		out.push(children);
-	}
-	return out;
-}
-
 export function placeChild(
 	parentDom,
 	childVNode,
