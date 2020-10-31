86599ced65bd21a13a42c3ae65c7a8e3395ebfb1
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-01 05:54:30 +0900

    del cjs, clone element

--- packages/oreact/js/index.js
+++ packages/oreact/js/index.js
@@ -1,14 +1,3 @@
-export { render, hydrate } from './render';
-export {
-	createElement,
-	createElement as h,
-	Fragment,
-	createRef,
-	isValidElement
-} from './create-element';
+export { render } from './render';
+export { createElement as h } from './create-element';
 export { Component } from './component';
-export { cloneElement } from './clone-element';
-export { createContext } from './create-context';
-export { toChildArray } from './diff/children';
-export { unmount as __u } from './diff';
-export { default as options } from './options';
