4cebb6a1a883076e02088889fcffab74b46f1de9
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-04 23:42:11 +0900

    build

--- packages/oreact/js/constants.js
+++ packages/oreact/js/constants.js
@@ -1,3 +1,3 @@
-export var EMPTY_OBJ = {};
-export var EMPTY_ARR = [];
-export var IS_NON_DIMENSIONAL = /acit|ex(?:s|g|n|p|$)|rph|grid|ows|mnc|ntw|ine[ch]|zoo|^ord|itera/i;
+export const EMPTY_OBJ = {};
+export const EMPTY_ARR = [];
+export const IS_NON_DIMENSIONAL = /acit|ex(?:s|g|n|p|$)|rph|grid|ows|mnc|ntw|ine[ch]|zoo|^ord|itera/i;
