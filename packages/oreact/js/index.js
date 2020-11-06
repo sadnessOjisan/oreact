9e777f4bd71f99588af92b100456421009d792ee
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-06 16:28:59 +0900

    rm js

--- packages/oreact/js/index.js
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-export { render } from "./render";
-export { createElement as h } from "./create-element";
-export { Component } from "./component";
-=======
-export { render } from './render';
-export { createElement as h } from './create-element';
-export { Component } from './component';
->>>>>>> main
